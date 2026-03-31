f59b4f1f1f390dbb52b68c4fe3f9321c71a9cbcf
Arun Shrestha <arunkshrestha3@gmail.com>
2026-03-31 16:58:11 -0700

    initial commit with all the necessary files created

---
+++ src/App.jsx
@@ -0,0 +1,92 @@
+import { useState } from "react";
+import "./App.css";
+
+export default function App() {
+  const [question, setQuestion] = useState("");
+  const [answer, setAnswer] = useState(
+    ""
+  );
+  const [loading, setLoading] = useState(false);
+
+  const handleAsk = async() => {
+    if (!question.trim()) return;
+    setAnswer("");
+    
+    setLoading(true);
+    try{
+      const res = await fetch("http://localhost:8000/ask",{
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({question}),
+      });
+
+      // const data = await res.json();
+      // setAnswer(data.answer);
+
+      const reader = res.body.getReader();
+      const decoder = new TextDecoder("utf-8");
+      let done = false
+
+        while (!done) {
+        const { value, done: doneReading } = await reader.read();
+        done = doneReading;
+
+        const chunk = decoder.decode(value);
+        setAnswer((prev) => prev + chunk);
+      }
+
+    }catch{
+      console.error(err);
+      setAnswer("Error connecting to server.");
+    }
+    setLoading(false);
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      handleAsk();
+    }
+  };
+
+  return (
+    <div className="page">
+      <div className="background-glow glow-1"></div>
+      <div className="background-glow glow-2"></div>
+
+      <main className="container">
+        <section className="hero">
+          <div className="nebula-wrapper">
+            <div className="nebula-core"></div>
+            <div className="nebula-ring ring-1"></div>
+            <div className="nebula-ring ring-2"></div>
+            <div className="nebula-ring ring-3"></div>
+          </div>
+        </section>
+        <section className="qa-card">
+          <div className="input-group">
+            <input
+              type="text"
+              placeholder="Type your question..."
+              value={question}
+              onChange={(e) => setQuestion(e.target.value)}
+              onKeyDown={handleKeyDown}
+            />
+            <button onClick={handleAsk} disabled={loading}>
+              {loading ? "Thinking..." : "Ask"}
+            </button>
+          </div>
+
+          <div className="answer-box">
+            <span className="answer-label">Answer</span>
+            <p>
+              {answer}
+              {loading && <span className="cursor">▌</span>}
+            </p>
+          </div>
+        </section>
+      </main>
+    </div>
+  );
+}
